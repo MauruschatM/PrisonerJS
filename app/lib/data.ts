36a12749b9baa087e3be4b3d1bd5358ce2d6871b
Gongure <klulk@outlook.de>
2025-07-25 00:50:54 +0200

    gfg

---
+++ app/lib/data.ts
@@ -0,0 +1,28 @@
+import db from "@/server/lib/db";
+import { tournaments } from "@/server/lib/db/schema";
+import { desc } from "drizzle-orm";
+
+export async function fetchTournaments() {
+    try {
+        const tournamentsData = await db
+            .select({
+                id: tournaments.id,
+                name: tournaments.name,
+                description: tournaments.description,
+                status: tournaments.status,
+                roundsPerMatch: tournaments.roundsPerMatch,
+                scheduledAt: tournaments.scheduledAt,
+                startedAt: tournaments.startedAt,
+                completedAt: tournaments.completedAt,
+                createdAt: tournaments.createdAt,
+            })
+            .from(tournaments)
+            .orderBy(desc(tournaments.createdAt))
+            .limit(20);
+
+        return tournamentsData;
+    } catch (error) {
+        console.error('Database Error:', error);
+        throw new Error('Failed to fetch revenue data.');
+    }
+}
